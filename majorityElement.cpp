2a591dd089cf626035ee77b7d00033392e66e6f9
Chandan Mishra <56648989+TheDarkKnight-1@users.noreply.github.com>
2021-07-01 09:52:23 +0530

    Create majorityElement.cpp

---
+++ majorityElement.cpp
@@ -0,0 +1,19 @@
+int majorityElement(vector<int>& nums) {
+        map<int,int>m;
+        int n=nums.size(),val=1;
+        for(int i=0;i<n;i++){
+            if(m.find(nums[i])==m.end()){
+                m.insert({nums[i],1});
+            }
+            else{
+                m[nums[i]]=m[nums[i]]+1;
+            }
+        }
+        for(auto i=m.begin();i!=m.end();++i){
+            //cout<<i->first<<" "<<i->second<<endl;
+            if((i->second)>floor(n/2)){
+                val=i->first;
+            }
+        }
+        return val;
+    }
